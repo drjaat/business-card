df0ada5c0116be4093a4f2403e866bc7f987e465
Dhruv Rana <22201670+drjaat@users.noreply.github.com>
2024-07-20 14:09:00 +0000

    business card

--- src/App.js
+++ src/App.js
@@ -1,25 +1,148 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
+import {
+  Typography,
+  AppBar,
+  Toolbar,
+  TextField,
+  Button,
+  Container,
+  Paper,
+} from "@mui/material";
+import * as React from "react";
 
-function App() {
+export default function App() {
+  const [name, setName] = React.useState("");
+  const [position, setPosition] = React.useState("");
+  const [company, setCompany] = React.useState("");
+  const [mobile, setMobile] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [address, setAddress] = React.useState("");
+  const [website, setWebsite] = React.useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    // Build the data string
+    const data = new URLSearchParams({
+      "Name": name,
+      "Designation": position,
+      "Company": company,
+      "Mobile": mobile,
+      "Email": email,
+      "Address": address,
+      "Website": website,
+    });
+
+    // Send the data to the Sheets API url
+    const response = await fetch("https://script.google.com/macros/s/AKfycbxD5JNJt-EVbUpCslhGlwh9yhMLlzpxALH_eBD7NQOi86vei3Gtl7uosXU3ZJJVMyfT/exec", {
+      method: "POST",
+      body: data.toString(),
+    });
+
+    if (!response.ok) {
+      console.error("Error submitting data:", response.statusText);
+      return;
+    }
+
+    // Clear form fields after successful submission (optional)
+    setName("");
+    setPosition("");
+    setCompany("");
+    setMobile("");
+    setEmail("");
+    setAddress(0);
+    setWebsite("");
+
+    alert("Data submitted successfully!");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="App">
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">
+            Submit Details to Sheet
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Container maxWidth="sm">
+        <Paper elevation={3} className="form-container">
+          <form className="form" onSubmit={handleSubmit}>
+            <TextField
+              fullWidth
+              margin="normal"
+              type="text"
+              label="Name"
+              variant="outlined"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <TextField
+              fullWidth
+              margin="normal"
+              type="text"
+              label="Designation"
+              variant="outlined"
+              value={position}
+              onChange={(e) => setPosition(e.target.value)}
+            />
+            <TextField
+              fullWidth
+              margin="normal"
+              type="text"
+              label="Company"
+              variant="outlined"
+              value={company}
+              onChange={(e) => setCompany(e.target.value)}
+            />
+            <TextField
+              fullWidth
+              margin="normal"
+              type=""
+              label="Mobile"
+              variant="outlined"
+              value={mobile}
+              onChange={(e) => setMobile(e.target.value)}
+            />
+            <TextField
+              fullWidth
+              margin="normal"
+              type="text"
+              label="Email"
+              variant="outlined"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <TextField
+              fullWidth
+              margin="normal"
+              type="text"
+              label="Address"
+              variant="outlined"
+              value={email}
+              onChange={(e) => setAddress(e.target.value)}
+            />
+            <TextField
+              fullWidth
+              margin="normal"
+              type="text"
+              label="Website"
+              variant="outlined"
+              value={website}
+              onChange={(e) => setWebsite(e.target.value)}
+            />
+            <Button
+              variant="contained"
+              color="primary"
+              type="submit"
+              className="submit-button"
+            >
+              Save
+            </Button>
+          </form>
+        </Paper>
+      </Container>
     </div>
   );
-}
-
-export default App;
+};
